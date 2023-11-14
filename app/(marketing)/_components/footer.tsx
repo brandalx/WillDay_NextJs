faac10ed6a16455288d49fab8a221c6dd30c2e7c
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-14 19:55:15 +0400

    update: footer layout

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -9,11 +9,16 @@ export const Footer = () => {
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-          <Button size="sm" variant="ghost">
+          <Button className="underline" size="sm" variant="ghost">
             Privacy Policy
           </Button>
+          <Button className="underline" size="sm" variant="ghost" asChild>
+            <Link target="_blank" href="https://brandnolandev.com">
+              About
+            </Link>
+          </Button>
 
-          <Button size="sm" variant="ghost">
+          <Button className="underline" size="sm" variant="ghost">
             Terms of Service
           </Button>
         </div>
