dfcf212dc349da1f656cb0926d43aab0ef5a9aca
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-15 03:31:25 +0400

    feat: OrganizationLayout layout

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
+      <div className="flex gap-x-7">
+        <div className="w-64 shrink-0 hidden md:block">
+          {/* todo: sidebar */}
+          {children}
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default OrganizationLayout;
