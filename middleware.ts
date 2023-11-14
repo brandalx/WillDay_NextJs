e903fd5ee19744cd649bafa5421ef15e4b4bd734
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-15 01:50:06 +0400

    feat: afterAuth middleware handler

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,20 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
-export default authMiddleware({ publicRoutes: ["/"] });
+export default authMiddleware({
+  publicRoutes: ["/"],
+  afterAuth(auth, req) {
+    if (auth.userId && auth.isPublicRoute) {
+      let path = "/select-org";
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
