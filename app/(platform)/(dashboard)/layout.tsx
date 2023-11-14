37a92298989ae62522bf699e2f30a8b0428c880e
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-15 01:20:29 +0400

    feat: Navbar for dahsboard init

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "./_components/navbar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>layout</div>;
+  return (
+    <div className="h-full">
+      <Navbar />
+      {children}
+    </div>
+  );
 };
 
 export default DashboardLayout;
