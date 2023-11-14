f228e0753fd50bc890c39c2afe91abc3e844b018
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-15 03:42:28 +0400

    feat: Sidebar init

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Sidebar from "../_components/sidebar";
 
 const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
       <div className="flex gap-x-7">
         <div className="w-64 shrink-0 hidden md:block">
-          {/* todo: sidebar */}
+          <Sidebar />
           {children}
         </div>
       </div>
