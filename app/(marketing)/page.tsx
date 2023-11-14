edadb1178ad01d2d68880af4157fe71fc0e939ac
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-14 19:17:34 +0400

    feat: hero page layout, custom fonts

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,28 @@
+import { Button } from "@/components/ui/button";
 import { Medal } from "lucide-react";
+import Link from "next/link";
 import React from "react";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2",
+});
+
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 const MarketingPage = () => {
   return (
     <div className="flex items-center justify-center flex-col ">
-      <div className="flex items-center justify-center flex-col">
+      <div
+        className={cn(
+          "flex items-center justify-center flex-col",
+          headingFont.className
+        )}
+      >
         <div className="mb-4 flex items-center  shadow-sm p-4 bg-purple-200 text-fuchsia-800 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
           No 1 in task management
@@ -17,11 +35,19 @@ const MarketingPage = () => {
           your daily time work
         </div>
       </div>
-      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-3xl text-center mx-auto ">
+      <div
+        className={cn(
+          "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-3xl text-center mx-auto ",
+          textFont.className
+        )}
+      >
         Collaborate, manage projects and products, and reach new heights in
         productivity. From towering skyscrapers to the convenience of your home
         office, the way your team works is unique. Achieve it all with WillDay
       </div>
+      <Button className="mt-6 " size="lg" asChild>
+        <Link href="/sign-up">Get WillDay for free</Link>
+      </Button>
     </div>
   );
 };
