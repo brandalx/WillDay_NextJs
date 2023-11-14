a2df6862cf4a93ae08c858c9c5bdc5d95d27fa49
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-15 03:38:49 +0400

    feat: dynamic change for params OrgControl

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
@@ -3,7 +3,17 @@ import { useEffect } from "react";
 import { useParams } from "next/navigation";
 import { useOrganizationList } from "@clerk/nextjs";
 const OrgControl = () => {
-  return <div>OrgControl</div>;
+  const params = useParams();
+  const { setActive } = useOrganizationList();
+
+  useEffect(() => {
+    if (!setActive) return;
+
+    setActive({
+      organization: params.organizationId as string,
+    });
+  }, [setActive, params.organizationId]);
+  return null;
 };
 
 export default OrgControl;
--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import OrgControl from "./_components/org-control";
 
