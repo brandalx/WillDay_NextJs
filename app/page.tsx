4f475b0d82e366cae3d2583092de27d6d2e37275
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-14 15:45:43 +0400

    test: dynamic params

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <div>WillDay</div>
-      init
-    </div>
-  );
+  return <div className="text-sky-500 ">WillDay</div>;
 }
