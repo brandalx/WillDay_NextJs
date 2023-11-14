5f2660596a0024e336618783b92b279ba9d52f62
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-15 01:46:29 +0400

    feat:navbar layout OrganizationSwitcher,UserButton

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,12 @@
 import Logo from "@/components/logo";
 import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { IconPlus } from "@tabler/icons-react";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="fixed z-50 px-4 top-0 w-full border-b shadow-sm bg-white flex items-center">
+    <nav className="fixed z-50 px-4 py-4 top-0 w-full border-b shadow-sm bg-white flex items-center">
       {/* todo: mobile sidebar */}
       <div className="flex items-center gap-x-4">
         <div className="hidden  md:flex ">
@@ -26,6 +27,34 @@ const Navbar = () => {
         >
           <IconPlus className="h-4 w-4" />
         </Button>
+      </div>
+      <div className="ml-auto flex items-center gap-x-2 ">
+        <OrganizationSwitcher
+          afterCreateOrganizationUrl="/organization/:id"
+          afterLeaveOrganizationUrl="/select-org"
+          afterSelectOrganizationUrl="/organization/:id"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+            },
+          }}
+        />
+
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: {
+                height: 30,
+                width: 30,
+              },
+            },
+          }}
+        />
       </div>
     </nav>
   );
