962fdb3a815f679c2ff8016984e5f653df113b12
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-14 19:57:31 +0400

    feat: siteConfig init

---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "WillDay",
+  description:
+    "Collaborate, manage projects and products, and reach new heights in productivity. From towering skyscrapers to the convenience of your home office, the way your team works is unique. Achieve it all with WillDay",
+};
