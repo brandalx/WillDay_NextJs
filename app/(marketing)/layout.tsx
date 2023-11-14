ff2799866c70d0c6d0b2cf150fa3d0a7f24b37df
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-14 18:56:09 +0400

    feat: layout children pass, hero page init, styles

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>MarketingLayout</div>;
+  return (
+    <div className="h-full bg-slate-100">
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      {/* todo: footer */}
+    </div>
+  );
 };
 
 export default MarketingLayout;
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,24 @@
+import { Medal } from "lucide-react";
 import React from "react";
 
 const MarketingPage = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex items-center justify-center flex-col ">
+      <div className="flex items-center justify-center flex-col">
+        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+          <Medal className="h-6 w-6 mr-2" />
+          No 1 task management
+        </div>
+        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+          Willday will help to organize
+        </h1>
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded mb pb-4 ">
+          {" "}
+          your daily time work
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MarketingPage;
