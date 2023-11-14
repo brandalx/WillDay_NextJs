2a78af0dc9f0f7a22680757ec6a0c5a34fc36571
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-15 03:34:22 +0400

    feat: OrgControl init

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { useOrganizationList } from "@clerk/nextjs";
+const OrgControl = () => {
+  return <div>OrgControl</div>;
+};
+
+export default OrgControl;
