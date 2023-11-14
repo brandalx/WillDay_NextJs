a90cb16cc9d2b477f6dfa776d786b9c9284e60fd
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-15 00:53:44 +0400

    feat: create organization page init

---
+++ app/(platform)/(clerk)/select-org/[[..select-org]]/page.tsx
@@ -0,0 +1,9 @@
+import { OrganizationList } from "@clerk/nextjs";
+
+import React from "react";
+
+const CreateOrganizationPage = () => {
+  return <div>CreateOrganizationPage</div>;
+};
+
+export default CreateOrganizationPage;
