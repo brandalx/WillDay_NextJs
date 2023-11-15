e069ab9cbc421037594702e69f2a64268fae10e7
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-15 15:29:58 +0400

    feat: mobilesidebar works

--- app/(platform)/(dashboard)/_components/MobileSidebar.tsx
+++ app/(platform)/(dashboard)/_components/MobileSidebar.tsx
@@ -1,8 +1,11 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { useMobileSidebar } from "@/hooks/use-mobile-sidebar";
+import { IconMenu, IconMenu2 } from "@tabler/icons-react";
 import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
-
+import { Sheet, SheetContent } from "@/components/ui/sheet";
+import Sidebar from "./sidebar";
 const MobileSidebar = () => {
   const pathname = usePathname();
   const [isMounted, setIsMounted] = useState(false);
@@ -13,8 +16,29 @@ const MobileSidebar = () => {
   useEffect(() => {
     setIsMounted(true);
   }, []);
+
+  useEffect(() => {
+    onClose();
+  }, [pathname, onClose]);
+
   if (!isMounted) return null;
-  return <div>MobileSidebar</div>;
+  return (
+    <>
+      <Button
+        variant="ghost"
+        size="sm"
+        className="block md:hidden mr-2"
+        onClick={onOpen}
+      >
+        <IconMenu2 className="h-4 w-4" />
+      </Button>
+      <Sheet open={isOpen} onOpenChange={onClose}>
+        <SheetContent side="left" className="p-2 pt-10">
+          <Sidebar storageKey="t-sidebar-mobile-state" />
+        </SheetContent>
+      </Sheet>
+    </>
+  );
 };
 
 export default MobileSidebar;
