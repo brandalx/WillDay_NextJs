eba65f7f3267202a332bb5d790bd97977e0a13fa
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-15 17:39:00 +0400

    test: server action works

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,27 @@
+// "use client";
 import React from "react";
 
 const OrganizationIdPage = () => {
-  return <div>Organizatin Page</div>;
+  async function create(formData: FormData) {
+    "use server";
+
+    console.log("triggered on server");
+  }
+  console.log("I am logged in the browser");
+
+  return (
+    <div>
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
