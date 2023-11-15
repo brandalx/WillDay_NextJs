1dab1f59899014081b2f418e78db2e5c9cf4198e
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-16 02:36:44 +0400

    feat: board elete action init

--- app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
@@ -1,13 +1,19 @@
+import { deleteBoard } from "@/actions/delete-board";
+import { Button } from "@/components/ui/button";
 import React from "react";
 interface BoardProps {
   title: string;
   id: string;
 }
 const Board = ({ title, id }: BoardProps) => {
+  const deleteBoardWithId = deleteBoard.bind(null, id);
   return (
-    <div className="space-y-2 ">
-      <div>Board title : {title}</div>
-    </div>
+    <form action={deleteBoardWithId} className="flex items-center gap-x-2  ">
+      <p className="">Board title : {title}</p>
+      <Button type="submit" variant="destructive" size="sm">
+        Delete
+      </Button>
+    </form>
   );
 };
 
