74c8dd19f0abd00b70f809c48c40638e09b0e7c7
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-15 04:17:50 +0400

    feat: Organization type

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,12 +1,25 @@
 "use-client";
 import React from "react";
+
+export type Organization = {
+  id: string;
+  slug: string;
+  imageUrl: string;
+  name: string;
+};
+
 interface NavItemProps {
   isActive: boolean;
   isExpanded: boolean;
   organization: any;
   onExpand: (id: string) => void;
 }
-const NavItem = () => {
+const NavItem = ({
+  isExpanded,
+  isActive,
+  organization,
+  onExpand,
+}: NavItemProps) => {
   return <div>NavItem</div>;
 };
 
