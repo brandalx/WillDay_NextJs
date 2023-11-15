318862f1c96374828a3e1c6c545da6454f559527
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-15 04:00:46 +0400

    feat: hooks for accordion,  defauklt vals

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -11,7 +11,28 @@ import { Accordion } from "@/components/ui/accordion";
 interface SidebarProps {
   storageKey?: string;
 }
-const Sidebar = ({ storageKey }: SidebarProps) => {
+const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization();
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+      return acc;
+    },
+    []
+  );
   return <div>Sidebar</div>;
 };
 
