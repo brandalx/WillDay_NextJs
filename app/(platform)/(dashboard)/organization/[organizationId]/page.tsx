636e8ebc79f2e3fe7b8ebb48e5676c6fc594f3f7
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-16 02:19:19 +0400

    feat: create board action init

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,21 +1,9 @@
 // "use client";
+import { create } from "@/actions/create-board";
 import { db } from "@/lib/db";
 import React from "react";
 
 const OrganizationIdPage = () => {
-  async function create(formData: FormData) {
-    "use server";
-
-    // console.log("triggered on server");
-    // const title = formData.get("title") as string;
-    // await db.board.create({
-    //   data: {
-    //     title,
-    //   },
-    // });
-  }
-  //   console.log("I am logged in the browser");
-
   return (
     <div>
       <form action={create}>
