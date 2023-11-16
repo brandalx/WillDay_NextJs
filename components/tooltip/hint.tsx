2d5894c87bcd1151c84cc7088065a3653a812a5f
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 00:47:08 +0400

    feat: hint comp for tooltip

--- components/tooltip/hint.tsx
+++ components/tooltip/hint.tsx
@@ -1,7 +1,36 @@
 import React from "react";
-
-const Hint = () => {
-  return <div>Hint</div>;
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+interface HintProps {
+  children: React.ReactNode;
+  description: string;
+  side?: "left" | "right" | "top" | "bottom";
+  sideOffset?: number;
+}
+const Hint = ({
+  children,
+  description,
+  side = "bottom",
+  sideOffset = 0,
+}: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent
+          sideOffset={sideOffset}
+          side={side}
+          className="text-xs max-w-[200px] break-words"
+        >
+          {description}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 };
 
 export default Hint;
