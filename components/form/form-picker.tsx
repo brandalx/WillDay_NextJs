a3c44150b12d138bed65c28daaa74ed905ea99a8
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 01:33:05 +0400

    feat: FormPicker init

---
+++ components/form/form-picker.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+interface FormPickerProps {
+  id: string;
+  error?: Record<string, string[] | undefined>;
+}
+
+import React from "react";
+
+const FormPicker = () => {
+  return <div>FormPicker</div>;
+};
+
+export default FormPicker;
