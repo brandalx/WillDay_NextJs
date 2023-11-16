49982cd00b4acc7dd277d8cbe544103573cb644b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 02:56:13 +0400

    feat: form popover for navbar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -4,7 +4,7 @@ import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { IconPlus } from "@tabler/icons-react";
 import React from "react";
 import { MobileSidebar } from "./MobileSidebar";
-
+import FormPopover from "@/components/form/form-popover";
 const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
@@ -16,14 +16,15 @@ const Navbar = () => {
         <div className="hidden md:flex">
           <Logo type="filled" />
         </div>
-
-        <Button
-          variant="primary"
-          size="sm"
-          className="rounded-sm hidden md:block h-auto py-1.5 px-2"
-        >
-          Create
-        </Button>
+        <FormPopover align="start" side="bottom" sideOffset={18}>
+          <Button
+            variant="primary"
+            size="sm"
+            className="rounded-sm hidden md:block h-auto py-1.5 px-2"
+          >
+            Create
+          </Button>
+        </FormPopover>
         <Button
           variant="primary"
           size="sm"
