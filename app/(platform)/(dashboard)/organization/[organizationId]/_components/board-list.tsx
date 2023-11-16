d2cc759f1f80dab441900263838f1f2f082bee9d
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 00:39:33 +0400

    feat: BoardList basic layout

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,7 +1,24 @@
+import { IconUser } from "@tabler/icons-react";
 import React from "react";
 
 const BoardList = () => {
-  return <div>BoardList</div>;
+  return (
+    <div className="space-y-4 ">
+      <div className="flex items-center font-semibold text-lg text-neutral-700">
+        <IconUser className="h-6 w-6 mr-2" />
+        Your boards
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div
+          role="button"
+          className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition-all"
+        >
+          <p className="text-sm"> Create new board</p>
+          <span className="text-xs">5 boards remaining</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BoardList;
