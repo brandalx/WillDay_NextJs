4bf5b1ccedbe3e6e6a539ec2e201704378449511
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-16 22:14:45 +0400

    feat:UseActionOptions interface for use actionhook

--- hooks/use-action.ts
+++ hooks/use-action.ts
@@ -4,3 +4,9 @@ import { ActionState, FieldErrors } from "@/lib/create-safe-action";
 type Action<TInput, TOutput> = (
   data: TInput
 ) => Promise<ActionState<TInput, TOutput>>;
+
+interface UseActionOptions<TOutput> {
+  onSuccess?: (data: TOutput) => void;
+  onError?: (error: string) => void;
+  onComplete?: () => void;
+}
