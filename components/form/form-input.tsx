626c9c20af52eb7f083312ba2c389928cb13d17e
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-16 22:37:51 +0400

    feat: reusble input, FormInputProps init

---
+++ components/form/form-input.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+interface FormInputProps {
+  id: string;
+  label?: string;
+  type?: string;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+
+  errors?: Record<string, string[] | undefined>;
+  className?: string;
+  defaultValue?: string;
+  onBlur?: () => void;
+}
