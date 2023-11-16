70f2a0074c1074c8c34aa93a03eac714ef9e7ae6
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-16 23:02:49 +0400

    feat: form submit buttonr reuseble comp, interface

---
+++ components/form/form-submit.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+
+interface FormSubmitProps {
+  children: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | "primary";
+}
