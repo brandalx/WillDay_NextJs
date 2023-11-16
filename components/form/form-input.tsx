a57dac4a3ec24109ed7cc08e7c0a54dc51cc2e21
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-16 22:42:25 +0400

    feat: form input props, label comp init

--- components/form/form-input.tsx
+++ components/form/form-input.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { forwardRef } from "react";
+import { useFormStatus } from "react-dom";
+
 interface FormInputProps {
   id: string;
   label?: string;
@@ -13,3 +16,30 @@ interface FormInputProps {
   defaultValue?: string;
   onBlur?: () => void;
 }
+
+export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
+  (
+    {
+      id,
+      label,
+      type,
+      placeholder,
+      required,
+      disabled,
+      erros,
+      className,
+      defaultValue = "",
+      onBlur,
+    },
+    ref
+  ) => {
+    const { pending } = useFormStatus();
+    return (
+      <div className="space-y-2">
+        <div className="space-y-1">{label ? <div>Label</div> : null}</div>
+      </div>
+    );
+  }
+);
+
+FormInput.displayName = "FormInput";
