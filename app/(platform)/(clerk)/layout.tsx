0cb52bf72598d30c1e30661a1b84c6c77e0d3d25
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 03:53:00 +0400

    update: ui updates

--- app/(platform)/(clerk)/layout.tsx
+++ app/(platform)/(clerk)/layout.tsx
@@ -1,10 +1,10 @@
 import Logo from "@/components/logo";
-import React from "react";
+import React, { useEffect } from "react";
 
 const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full flex items-center justify-center flex-col">
-      <div className="my-4">
+      <div className="my-4 flex">
         <Logo isInLine={true} type="filled" />
       </div>
       {children}
