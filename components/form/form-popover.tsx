3d2e95b1b5203c2bba70a8486bd85c1d984e399a
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 01:02:56 +0400

    feat: popover close

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,6 +1,7 @@
 "use client";
 import {
   Popover,
+  PopoverClose,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
@@ -10,6 +11,8 @@ import { CreateBoard } from "@/actions/create-board/schema";
 import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
 import React from "react";
+import { Button } from "../ui/button";
+import { IconX } from "@tabler/icons-react";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -36,6 +39,14 @@ const FormPopover = ({
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           Create Board
         </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600 "
+            variant="ghost"
+          >
+            <IconX className="h-4 w-4 " />
+          </Button>
+        </PopoverClose>
       </PopoverContent>
     </Popover>
   );
