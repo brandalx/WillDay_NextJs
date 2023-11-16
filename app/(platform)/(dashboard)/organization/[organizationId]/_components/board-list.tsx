9de54bc7ff635f63595d7623130adf515e8cb437
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 00:58:48 +0400

    feat: form popover usage

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,3 +1,4 @@
+import FormPopover from "@/components/form/form-popover";
 import Hint from "@/components/tooltip/hint";
 import { IconHelpCircle, IconUser } from "@tabler/icons-react";
 import React from "react";
@@ -10,19 +11,21 @@ const BoardList = () => {
         Your boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-        <div
-          role="button"
-          className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition-all"
-        >
-          <p className="text-sm"> Create new board</p>
-          <span className="text-xs">5 boards remaining</span>
-          <Hint
-            sideOffset={40}
-            description={`Free workspaces can have up to 5 open boards. For unlimited experience upgrade this workspace.`}
+        <FormPopover sideOffset={10} side="right">
+          <div
+            role="button"
+            className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition-all"
           >
-            <IconHelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
-          </Hint>
-        </div>
+            <p className="text-sm"> Create new board</p>
+            <span className="text-xs">5 boards remaining</span>
+            <Hint
+              sideOffset={40}
+              description={`Free workspaces can have up to 5 open boards. For unlimited experience upgrade this workspace.`}
+            >
+              <IconHelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
+            </Hint>
+          </div>
+        </FormPopover>
       </div>
     </div>
   );
