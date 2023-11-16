6fa88bd2eae95d6e4b31c42db7d9bcacf39e4d7f
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-16 21:55:08 +0400

    feat: createSafeAction on return field errors

--- lib/create-safe-action.ts
+++ lib/create-safe-action.ts
@@ -8,3 +8,18 @@ export type ActionState<TInput, TOutput> = {
   error?: string | null;
   data?: TOutput;
 };
+
+export const createSafeAction = <TInput, TOutput>(
+  schema: z.Schema<TInput>,
+  handler: (validateData: TInput) => Promise<ActionState<TInput, TOutput>>
+) => {
+  return async (data: TInput): Promise<ActionState<TInput, TOutput>> => {
+    const validationResult = schema.safeParse(data);
+    if (!validationResult.success) {
+      return {
+        fieldErrors: validationResult.error.flatten()
+          .fieldErrors as FieldErrors<TInput>,
+      };
+    }
+  };
+};
