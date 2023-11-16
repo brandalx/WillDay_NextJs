21d29ab891202e7b57e0c4bed2615b6b1b32cf1e
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-16 21:49:54 +0400

    feat: action state

--- lib/create-safe-action.ts
+++ lib/create-safe-action.ts
@@ -3,3 +3,8 @@ import { z } from "zod";
 export type FieldErrors<T> = {
   [K in keyof T]?: string[];
 };
+export type ActionState<TInput, TOutput> = {
+  fieldErrors?: FieldErrors<TInput>;
+  error?: string | null;
+  data?: TOutput;
+};
