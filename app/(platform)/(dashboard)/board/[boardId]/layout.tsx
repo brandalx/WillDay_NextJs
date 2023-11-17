37c7a10d949df9743e649e7d2ed4a6cf25e7ae4b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 15:19:59 +0400

    feat: board layout

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -1,7 +1,37 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { notFound, redirect } from "next/navigation";
 import React from "react";
 
-const BoardIdLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+const BoardIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { boardId: string };
+}) => {
+  const { orgId } = auth();
+  if (!orgId) {
+    redirect("/select-org");
+  }
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  if (!board) {
+    notFound();
+  }
+  return (
+    <div
+      className="relative h-full bg-no-repeat bg-cover bg-center "
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+    >
+      <main className="relative pt-28 h-full">{children}</main>
+    </div>
+  );
 };
 
 export default BoardIdLayout;
