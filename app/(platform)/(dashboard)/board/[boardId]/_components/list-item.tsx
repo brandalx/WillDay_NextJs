f91a4c674d7fa15fe677821b8ebde10c5e36aa2e
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-18 00:52:39 +0400

    feat: layout, data pass for ListHeader

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -7,11 +7,11 @@ interface ListItemProps {
   data: ListWithCards;
   index: number;
 }
-const ListItem = () => {
+const ListItem = ({ data, index }: ListItemProps) => {
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
-      <div className="w-full rounded-md bg-[#1F1F2F4]shadow-md pb-2">
-        <ListHeader />
+      <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2 ">
+        <ListHeader data={data} />
       </div>
     </li>
   );
