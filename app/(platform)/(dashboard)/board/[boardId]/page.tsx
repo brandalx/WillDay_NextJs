05922f4445730462aee080dce64aa2637e2ed3c6
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 22:47:39 +0400

    feat: fetching board lists, cards

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,6 +1,35 @@
 import React from "react";
-
-const BoardIdPage = () => {
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { orgId } = auth();
+  if (!orgId) {
+    redirect("/select-org");
+  }
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
   return <div>BoardIdPage</div>;
 };
 
