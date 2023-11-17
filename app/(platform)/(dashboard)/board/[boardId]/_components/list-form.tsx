bdef63a8116e356fa77446cbaeebcd03a765a430
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 23:02:09 +0400

    feat:states, refs for form

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { IconPlus } from "@tabler/icons-react";
 import ListWrapper from "./list-wrapper";
+import { useState, useRef, ElementRef } from "react";
 export const ListForm = () => {
+  const { isEditing, setIsEditing } = useState(false);
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
   return (
     <ListWrapper>
       <button className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm">
