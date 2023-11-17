40f8acbb83cd17e9a9f969d1b08ce35ff71152eb
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 23:37:17 +0400

    feat:last list def, creation of list, order assign

--- actions/create-list/index.ts
+++ actions/create-list/index.ts
@@ -1,12 +1,12 @@
 "use server";
 
 import { auth } from "@clerk/nextjs";
-import { InputType } from "../update-board/types";
+import { InputType } from "./types";
 import { c } from "@/lib/console-log";
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
-import { UpdateBoard } from "./schema";
+import { CreateList } from "./schema";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -15,27 +15,46 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       error: "Unauthorized",
     };
   }
-  const { title, id } = data;
-  let board;
+  const { title, boardId } = data;
+  let list;
 
   try {
-    board = await db.board.update({
+    const board = await db.board.findUnique({
       where: {
-        id,
+        id: boardId,
         orgId,
       },
+    });
+
+    if (!board) {
+      return {
+        error: "Board not found",
+      };
+    }
+
+    const lastList = await db.list.findFirst({
+      where: { boardId: boardId },
+      orderBy: { order: "desc" },
+      select: { order: true },
+    });
+
+    const newOrder = lastList ? lastList.order + 1 : 1;
+
+    list = await db.list.create({
       data: {
         title,
+        boardId,
+        order: newOrder,
       },
     });
   } catch (error) {
     c(error);
     return {
-      error: "failed to update",
+      error: "failed to create",
     };
   }
-  revalidatePath(`/board/${id}`);
-  return { data: board };
+  revalidatePath(`/board/${boardId}`);
+  return { data: list };
 };
 
-export const updateBoard = createSafeAction(UpdateBoard, handler);
+export const createList = createSafeAction(CreateList, handler);
