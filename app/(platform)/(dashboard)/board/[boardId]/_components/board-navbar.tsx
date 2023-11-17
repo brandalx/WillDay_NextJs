9f01b73d389b59c6a6960040f0ac4e176f557f63
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 15:33:53 +0400

    cleanup: dry

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,18 +1,13 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import { Board } from "@prisma/client";
 import React from "react";
 interface BoardNavbarProps {
-  id: string;
+  data: Board;
 }
-export const BoardNavbar = async ({ id }: BoardNavbarProps) => {
+export const BoardNavbar = async ({ data }: BoardNavbarProps) => {
   const { orgId } = auth();
 
-  const board = await db.board.findUnique({
-    where: {
-      id: id,
-      orgId: orgId!,
-    },
-  });
   return (
     <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
       board-navbar
