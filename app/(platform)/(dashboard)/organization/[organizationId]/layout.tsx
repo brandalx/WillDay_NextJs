ac71d95d96d5dd8f246e8d38c20a29fdd4bb4f93
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 15:09:58 +0400

    feat: generateMetadata for current org

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,6 +1,13 @@
-"use client";
-import React from "react";
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
 import OrgControl from "./_components/org-control";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
