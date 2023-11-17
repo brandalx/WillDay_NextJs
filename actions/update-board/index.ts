cac11d5bf4f50537a6cb2e55a68657a9a54183c7
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-17 16:07:02 +0400

    feat: handler for update board

---
+++ actions/update-board/index.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { InputType } from "../update-board/types";
+import { c } from "@/lib/console-log";
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+  const { title, id } = data;
+  let board;
+
+  try {
+    board = await db.board.update({
+      where: {
+        id,
+        orgId,
+      },
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    c(error);
+    return {
+      error: "failed to update",
+    };
+  }
+  revalidatePath(`/board/${id}`);
+  return { data: board };
+};
