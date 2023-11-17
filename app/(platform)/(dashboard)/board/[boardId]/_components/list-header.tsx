50cad51a9e443cb7b55b51c1921cc5cd7f5a00ac
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-18 01:02:44 +0400

    feat: isEditing cause layout

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -3,6 +3,7 @@ import { useState, useRef, ElementRef } from "react";
 import { List } from "@prisma/client";
 import React from "react";
 import { useEventListener } from "usehooks-ts";
+import { FormInput } from "@/components/form/form-input";
 interface ListHeaderProps {
   data: List;
 }
@@ -34,7 +35,17 @@ const ListHeader = ({ data }: ListHeaderProps) => {
   return (
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
       {isEditing ? (
-        <p>Form</p>
+        <form className="flex-1 px-[2px]">
+          <input hidden id="id" name="id" value={data.id} />
+          <input hidden id="boardId" name="boardId" value={data.boardId} />
+          <FormInput
+            ref={inputRef}
+            onBlur={() => {}}
+            id="title"
+            placeholder="Enter list title..."
+            defaultValue={title}
+          />
+        </form>
       ) : (
         <div
           onClick={enableEditing}
