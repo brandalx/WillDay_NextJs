d9e81c6a36edbd312fd3ed7de980c0b9f741d16d
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-18 16:48:50 +0400

    feat: onSubmit, execution

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -14,6 +14,8 @@ import React, {
   ElementRef,
   KeyboardEventHandler,
 } from "react";
+import { toast } from "sonner";
+import { error } from "console";
 interface CardFormProps {
   listId: string;
   isEditing: boolean;
@@ -24,7 +26,15 @@ const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
   ({ listId, disableEditing, enableEditing, isEditing }, ref) => {
     const params = useParams();
     const formRef = useRef<ElementRef<"form">>(null);
-    const { execute, fieldErrors } = useAction(createCard);
+    const { execute, fieldErrors } = useAction(createCard, {
+      onSuccess: (data: any) => {
+        toast.success(`Card "${data.title}" created`);
+        formRef.current?.reset();
+      },
+      onError: (error) => {
+        toast.error(error);
+      },
+    });
     const onKeyDown = (e: KeyboardEvent) => {
       if (e.key === "Escape") {
         disableEditing();
@@ -41,14 +51,27 @@ const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
       }
     };
 
+    const onSubmit = (formData: FormData) => {
+      const title = formData.get("title") as string;
+      const listId = formData.get("listId") as string;
+      const boardId = formData.get("boardId") as string;
+
+      execute({ title, listId, boardId });
+    };
+
     if (isEditing) {
       return (
-        <form className="m-1 py-0.5 px-1 space-y-4">
+        <form
+          ref={formRef}
+          action={onSubmit}
+          className="m-1 py-0.5 px-1 space-y-4"
+        >
           <FormTextarea
             id="title"
-            onKeyDown={() => {}}
+            onKeyDown={onTextAreaKeyDown}
             ref={ref}
             placeholder="Enter a title for this card..."
+            errors={fieldErrors}
           />
           <input hidden id="listId" name="lisyId" value={listId} />
           <div className="flex items-center gap-x-1 ">
