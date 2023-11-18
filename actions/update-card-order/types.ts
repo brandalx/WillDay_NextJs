a2d0ba5e7f060bb99d274c72af94fdda09b49acd
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-18 19:59:50 +0400

    feat: types for update cards

--- actions/update-card-order/types.ts
+++ actions/update-card-order/types.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 import { ActionState } from "@/lib/create-safe-action";
-import { UpdateListOrder } from "./schema";
-import { List } from "@prisma/client";
-export type InputType = z.infer<typeof UpdateListOrder>;
-export type ReturnType = ActionState<InputType, List[]>;
+import { UpdateCardOrder } from "./schema";
+import { Card, List } from "@prisma/client";
+export type InputType = z.infer<typeof UpdateCardOrder>;
+export type ReturnType = ActionState<InputType, Card[]>;
