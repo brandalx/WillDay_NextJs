560caf60c43ad7b29612b3bb0239999cf0409d50
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-18 23:05:41 +0400

    feat:card modal initial states, usage usemodalhook

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -1,7 +1,15 @@
 " use client";
 
-import { Dialog } from "@/components/ui/dialog";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { useCardModal } from "@/hooks/use-card-modal";
 
 export const CardModal = () => {
-  return <Dialog></Dialog>;
+  const id = useCardModal((state) => state.id);
+  const isOpen = useCardModal((state) => state.isOpen);
+  const onClose = useCardModal((state) => state.onClose);
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent>Modal</DialogContent>
+    </Dialog>
+  );
 };
