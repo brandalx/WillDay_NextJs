5a6db4d9cc699fb7d9b6c5fbcd386e2c75298fe1
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-18 14:02:14 +0400

    feat: popover ui for options list

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -2,12 +2,33 @@
 
 import { List } from "@prisma/client";
 import React from "react";
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { IconDots } from "@tabler/icons-react";
 interface ListOptionsProps {
   data: List;
   onAddCard: () => void;
 }
 const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
-  return <div>ListOptions</div>;
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="h-auto w-auto p-2" variant="ghost">
+          <IconDots className="H-4 W-4" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="px-0 pt-3 pb-3" side="bottom" align="start">
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          List actions
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
 };
 
 export default ListOptions;
