f862492ce7d03ad857f2b993a21c2220871674d6
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-18 19:18:38 +0400

    feat: dragndrop func for card item

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,18 +1,26 @@
 "use client";
 import { Card } from "@prisma/client";
 import React from "react";
+import { Draggable } from "@hello-pangea/dnd";
 interface CardItemProps {
   index: number;
   data: Card;
 }
 const CardItem = ({ data, index }: CardItemProps) => {
   return (
-    <div
-      role="button"
-      className="truncate border-2 border-transparent hover:border-neutral-700 transition-all py-2 px-3 text-sm bg-white rounded-md shadow-sm "
-    >
-      {data.title}
-    </div>
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          role="button"
+          className="truncate border-2 border-transparent hover:border-neutral-700 transition-all py-2 px-3 text-sm bg-white rounded-md shadow-sm "
+        >
+          {data.title}
+        </div>
+      )}
+    </Draggable>
   );
 };
 
