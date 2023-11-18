8e24afdeb6e7ee0ba9b276fe2f1245ce1420473f
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-19 00:26:09 +0400

    feat: deps, refs, params, onBlur, onSubmit

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -4,18 +4,33 @@ import { FormInput } from "@/components/form/form-input";
 import { Skeleton } from "@/components/ui/skeleton";
 import { CardWithList } from "@/types";
 import { IconLayout2 } from "@tabler/icons-react";
-import React, { useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import React, { ElementRef, useRef, useState } from "react";
 interface HeaderProps {
   data: CardWithList;
 }
 export const Header = ({ data }: HeaderProps) => {
+  const queryClient = useQueryClient();
+  const params = useParams();
+  const inputRef = useRef<ElementRef<"input">>(null);
   const [title, setTitle] = useState(data?.title);
+  const onBlur = () => {
+    inputRef.current?.form?.requestSubmit();
+  };
+
+  const onSubmit = (formData: FormData) => {
+    console.log(formData.get("title"));
+  };
+
   return (
     <div className="flex items-start gap-x-3 mb-6 w-full">
       <IconLayout2 className="h-5 w-5 mt-1 text-neutral-700" />
       <div className="w-full">
-        <form>
+        <form action={onSubmit}>
           <FormInput
+            ref={inputRef}
+            onBlur={onBlur}
             className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate "
             defaultValue={title}
             id="title"
