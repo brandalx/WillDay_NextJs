1461c86c35d4461f13357413d9bf440c65371cce
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-18 16:07:05 +0400

    feat: add a card layout

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -1,19 +1,32 @@
 "use lcient";
 
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { IconPlus } from "@tabler/icons-react";
+import React, { forwardRef } from "react";
 interface CardFormProps {
   listId: string;
   isEditing: boolean;
   enableEditing: () => void;
   disableEditing: () => void;
 }
-const CardForm = ({
-  listId,
-  disableEditing,
-  enableEditing,
-  isEditing,
-}: CardFormProps) => {
-  return <div>CardForm</div>;
-};
+const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
+  ({ listId, disableEditing, enableEditing, isEditing }, ref) => {
+    return (
+      <div className="pt-2 px-2 ">
+        <Button
+          onClick={enableEditing}
+          className="h-auto px-2 py-1.5 w-full justify-start text-muted-foreground text-sm"
+          size="sm"
+          variant="ghost"
+        >
+          <IconPlus className="h-4 w-4 mr-2 " />
+          Add a card
+        </Button>
+      </div>
+    );
+  }
+);
+
+CardForm.displayName = "CardForm";
 
 export default CardForm;
