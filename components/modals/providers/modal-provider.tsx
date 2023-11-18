08947284e503a6d7e91ff85626e8779ef945dac3
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-18 23:09:47 +0400

    feat: preventing hydration

--- components/modals/providers/modal-provider.tsx
+++ components/modals/providers/modal-provider.tsx
@@ -1,6 +1,17 @@
 "use client";
+import { useEffect, useState } from "react";
 import { CardModal } from "../card-modal";
 export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <>
       <CardModal />
