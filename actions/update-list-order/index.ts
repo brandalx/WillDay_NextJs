fc3e6b851e5ecfd4c0961d6607e27a3434148c23
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-18 19:53:45 +0400

    feat: server action for reordering lists

--- actions/update-list-order/index.ts
+++ actions/update-list-order/index.ts
@@ -6,7 +6,7 @@ import { c } from "@/lib/console-log";
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
-import { CreateList } from "./schema";
+import { UpdateListOrder } from "./schema";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -15,46 +15,33 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       error: "Unauthorized",
     };
   }
-  const { title, boardId } = data;
-  let list;
+  const { items, boardId } = data;
+  let lists;
 
   try {
-    const board = await db.board.findUnique({
-      where: {
-        id: boardId,
-        orgId,
-      },
-    });
+    const transaction = items.map((list) =>
+      db.list.update({
+        where: {
+          id: list.id,
+          board: {
+            orgId,
+          },
+        },
+        data: {
+          order: list.order,
+        },
+      })
+    );
 
-    if (!board) {
-      return {
-        error: "Board not found",
-      };
-    }
-
-    const lastList = await db.list.findFirst({
-      where: { boardId: boardId },
-      orderBy: { order: "desc" },
-      select: { order: true },
-    });
-
-    const newOrder = lastList ? lastList.order + 1 : 1;
-
-    list = await db.list.create({
-      data: {
-        title,
-        boardId,
-        order: newOrder,
-      },
-    });
+    lists = await db.$transaction(transaction);
   } catch (error) {
     c(error);
     return {
-      error: "failed to create",
+      error: "failed to reorder",
     };
   }
   revalidatePath(`/board/${boardId}`);
-  return { data: list };
+  return { data: lists };
 };
 
-export const createList = createSafeAction(CreateList, handler);
+export const updateListOrder = createSafeAction(UpdateListOrder, handler);
