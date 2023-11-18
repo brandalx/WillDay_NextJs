a08914926a968375f45806323321a93430ecae62
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-18 23:56:16 +0400

    feat :auth case

--- app/api/cards/[cardId]/route.ts
+++ app/api/cards/[cardId]/route.ts
@@ -1,3 +1,4 @@
+import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export async function GET(
@@ -5,6 +6,9 @@ export async function GET(
   { params }: { params: { cardId: string } }
 ) {
   try {
+    const { userId, orgId } = auth();
+    if (!userId || !orgId)
+      return new NextResponse("Unauthorized", { status: 401 });
   } catch (error) {
     return new NextResponse("Internal Error", { status: 500 });
   }
