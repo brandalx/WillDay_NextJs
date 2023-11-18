4aaaf38ce377cf1f8ca82a80ff61933b5161faac
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-18 23:00:37 +0400

    feat: use card modal props, hook

--- hooks/use-card-modal.ts
+++ hooks/use-card-modal.ts
@@ -1,13 +1,15 @@
 import { create } from "zustand";
 
-type MobileSidebarStore = {
+type CardModalStore = {
+  id?: string;
   isOpen: boolean;
-  onOpen: () => void;
+  onOpen: (id: string) => void;
   onClose: () => void;
 };
 
-export const useMobileSidebar = create<MobileSidebarStore>((set) => ({
+export const useMobileSidebar = create<CardModalStore>((set) => ({
+  id: undefined,
   isOpen: false,
-  onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: false }),
+  onOpen: (id: string) => set({ isOpen: true, id }),
+  onClose: () => set({ isOpen: false, id: undefined }),
 }));
