97b43169eb2a526f687dc6143d098fe42cac9deb
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-18 15:54:48 +0400

    fix: rollback

--- app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -1,21 +1,8 @@
-"use client";
 import { OrganizationList } from "@clerk/nextjs";
 
 import React, { useEffect } from "react";
 
 const CreateOrganizationPage = () => {
-  useEffect(() => {
-    const deleteDivWithDelay = () => {
-      setTimeout(() => {
-        const divToDelete = document.querySelector(".cl-internal-b3fm6y");
-        if (divToDelete) {
-          divToDelete.remove();
-        }
-      }, 1000);
-    };
-
-    deleteDivWithDelay();
-  }, []);
   return (
     <OrganizationList
       hidePersonal
--- app/(platform)/(clerk)/sign-up/[[...sign-up]]/page.tsx
+++ app/(platform)/(clerk)/sign-up/[[...sign-up]]/page.tsx
@@ -1,19 +1,19 @@
-"use client";
+// "use client";
 import { SignUp } from "@clerk/nextjs";
 import { useEffect } from "react";
 
 export default function Page() {
-  useEffect(() => {
-    const deleteDivWithDelay = () => {
-      setTimeout(() => {
-        const divToDelete = document.querySelector(".cl-internal-b3fm6y");
-        if (divToDelete) {
-          divToDelete.remove();
-        }
-      }, 1000);
-    };
+  // useEffect(() => {
+  //   const deleteDivWithDelay = () => {
+  //     setTimeout(() => {
+  //       const divToDelete = document.querySelector(".cl-internal-b3fm6y");
+  //       if (divToDelete) {
+  //         divToDelete.remove();
+  //       }
+  //     }, 1000);
+  //   };
 
-    deleteDivWithDelay();
-  }, []);
+  //   deleteDivWithDelay();
+  // }, []);
   return <SignUp />;
 }
