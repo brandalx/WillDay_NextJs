e96d1374799d24cf469b3558075cfe13474b352c
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-20 02:07:24 +0400

    feat: getAvailableCount helper

--- lib/orgLimits.ts
+++ lib/orgLimits.ts
@@ -65,3 +65,17 @@ export const hasAvailableCount = async () => {
     return false;
   }
 };
+
+export const getAvailableCount = async () => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return 0;
+  }
+  const orgLimit = await db.orgLimit.findUnique({
+    where: { orgId },
+  });
+  if (!orgLimit) return 0;
+
+  return orgLimit.count;
+};
