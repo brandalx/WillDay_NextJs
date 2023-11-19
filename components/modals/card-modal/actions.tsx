77dc8c8ef29458f0328c1dfb52466f2a044b4408
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-19 19:08:27 +0400

    feat: button varian, size, actions layout

--- components/modals/card-modal/actions.tsx
+++ components/modals/card-modal/actions.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { CardWithList } from "@/types";
+import { IconCopy, IconTrash } from "@tabler/icons-react";
 import React from "react";
 
 interface ActionsProps {
@@ -9,8 +11,18 @@ interface ActionsProps {
 
 const Actions = ({ data }: ActionsProps) => {
   return (
-    <div>
-      <p>Actions</p>
+    <div className="space-y-2 mt-2">
+      <p className="text-xs font-semibold">Actions</p>
+      <Button size="inline" className="w-full justify-start" variant="gray">
+        <IconCopy className="h-4 w-4 mr-2" /> Copy
+      </Button>
+      <Button
+        size="inline"
+        className="w-full justify-start hover:bg-rose-300 text-white bg-rose-500"
+        variant="gray"
+      >
+        <IconTrash className="h-4 w-4 mr-2 " /> Delete
+      </Button>
     </div>
   );
 };
