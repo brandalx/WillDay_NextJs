4416c35e4c79a50f04d768fa3c376d818b23c6ac
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-20 02:17:34 +0400

    feat: decreaseAvailableCount on board delete

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -10,6 +10,7 @@ import { DeleteBoard } from "./schema";
 import { redirect } from "next/navigation";
 import { ReturnType } from "./types";
 import { createAuditLog } from "@/lib/create-audit-log";
+import { decreaseAvailableCount } from "@/lib/orgLimits";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -28,6 +29,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         orgId,
       },
     });
+    await decreaseAvailableCount();
 
     await createAuditLog({
       entityTitle: board.title,
