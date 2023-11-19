344d7ec3ff0ddd24b7bc475715dd10ef5620641d
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-20 02:29:52 +0400

    feat: subscription check for stripe

---
+++ lib/subscription.ts
@@ -0,0 +1,30 @@
+import { auth } from "@clerk/nextjs";
+import { db } from "./db";
+const DAY_IN_MS = 84_400_00;
+
+export const checkSubscription = async () => {
+  const { orgId } = auth();
+  if (!orgId) {
+    return false;
+  }
+
+  const orgSubscription = await db.orgSubscription.findUnique({
+    where: {
+      orgId,
+    },
+    select: {
+      stripeSubscriptionId: true,
+      stripeCustomerId: true,
+      stripeCurretPeriodEnd: true,
+      stripePriceId: true,
+    },
+  });
+  if (!orgSubscription) {
+    return false;
+  }
+  const isValid =
+    orgSubscription.stripePriceId &&
+    orgSubscription.stripeCurretPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+
+  return !!isValid;
+};
