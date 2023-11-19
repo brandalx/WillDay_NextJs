f2acb577fd0de63931dac20519efc3870b1523e8
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-20 02:05:37 +0400

    feat: hasAvailableCount

--- lib/orgLimits.ts
+++ lib/orgLimits.ts
@@ -48,3 +48,20 @@ export const decreaseAvailableCount = async () => {
     });
   }
 };
+
+export const hasAvailableCount = async () => {
+  const { orgId } = auth();
+  if (!orgId) {
+    throw new Error("Unauthorized");
+  }
+
+  const orgLimit = await db.orgLimit.findUnique({
+    where: { orgId },
+  });
+
+  if (!orgLimit || orgLimit.count < MAX_FREE_BOARDS) {
+    return true;
+  } else {
+    return false;
+  }
+};
