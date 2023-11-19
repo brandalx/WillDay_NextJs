edf34c24fe8e7df2d8d210c5d61316dbcf6e372f
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-20 00:26:51 +0400

    feat: auditLogsData fetching

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -7,6 +7,7 @@ import { fetcher } from "@/lib/fetcher";
 import { Header } from "./header";
 import { Description } from "./description";
 import Actions from "./actions";
+import { AuditLog } from "@prisma/client";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -15,6 +16,11 @@ export const CardModal = () => {
   const { data: cardData } = useQuery<CardWithList>({
     queryKey: ["card", id],
     queryFn: () => fetcher(`/api/cards/${id}`),
+  });
+
+  const { data: auditLogsData } = useQuery<AuditLog[]>({
+    queryKey: ["card-logs", id],
+    queryFn: () => fetcher(`/api/cards/${id}/logs`),
   });
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
