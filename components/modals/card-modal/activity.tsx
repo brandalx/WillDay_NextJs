f88ce8e0451b5f68a7f8f4f3bed4b2398f2409f3
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-20 01:16:44 +0400

    fix: activity comp return

--- components/modals/card-modal/activity.tsx
+++ components/modals/card-modal/activity.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ActivityItem } from "@/components/activity-item";
 import { Skeleton } from "@/components/ui/skeleton";
 import { AuditLog } from "@prisma/client";
 import { IconActivity } from "@tabler/icons-react";
@@ -15,9 +16,7 @@ export const Activity = ({ items }: ActivityProps) => {
         <p className="font-semibold text-neutral-700 mb-2 ">Activity</p>
         <ol className="mt-2 space-y-4">
           {items.map((items) => (
-            <p key={items.id} className="">
-              {items.entityTitle}
-            </p>
+            <ActivityItem key={items.id} data={items} />
           ))}
         </ol>
       </div>
