d644022a21a7617ba781ff92dc817018c9c23360
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-20 00:43:40 +0400

    feat: activity ui, data print

--- components/modals/card-modal/activity.tsx
+++ components/modals/card-modal/activity.tsx
@@ -2,12 +2,27 @@
 
 import { Skeleton } from "@/components/ui/skeleton";
 import { AuditLog } from "@prisma/client";
+import { IconActivity } from "@tabler/icons-react";
 import React from "react";
 interface ActivityProps {
   items: AuditLog[];
 }
 export const Activity = ({ items }: ActivityProps) => {
-  return <div>activity</div>;
+  return (
+    <div className="flex items-start gap-x-3 w-full">
+      <IconActivity className="h-5 w-5 mt-0.5 text-neutral-700" />
+      <div className="w-full">
+        <p className="font-semibold text-neutral-700 mb-2 ">Activity</p>
+        <ol className="mt-2 space-y-4">
+          {items.map((items) => (
+            <p key={items.id} className="">
+              {items.entityTitle}
+            </p>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
 };
 
 Activity.Skeleton = function ActivitySkeleton() {
