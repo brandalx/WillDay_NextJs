e12a40f2a84e94f97311c3e62db235684431d49f
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-19 23:55:47 +0400

    feat: delete and copy works

--- components/modals/card-modal/actions.tsx
+++ components/modals/card-modal/actions.tsx
@@ -8,6 +8,9 @@ import { copyCard } from "@/actions/copy-card/index";
 import { deleteCard } from "@/actions/delete-card";
 import React from "react";
 import { useParams } from "next/navigation";
+import { useCardModal } from "@/hooks/use-card-modal";
+import { toast } from "sonner";
+import { error } from "console";
 
 interface ActionsProps {
   data: CardWithList;
@@ -15,10 +18,31 @@ interface ActionsProps {
 
 const Actions = ({ data }: ActionsProps) => {
   const params = useParams();
-  const { execute: executeCopyCard, isLoading: isLoadingCopy } =
-    useAction(copyCard);
-  const { execute: executeDeleteCard, isLoading: isLoadingDelete } =
-    useAction(deleteCard);
+  const cardModal = useCardModal();
+  const { execute: executeCopyCard, isLoading: isLoadingCopy } = useAction(
+    copyCard,
+    {
+      onSuccess: (data) => {
+        toast.success(`Card "${data.title} duplicate`);
+        cardModal.onClose();
+      },
+      onError: (error) => {
+        toast.error(error);
+      },
+    }
+  );
+  const { execute: executeDeleteCard, isLoading: isLoadingDelete } = useAction(
+    deleteCard,
+    {
+      onSuccess: (data) => {
+        toast.success(`Card "${data.title} deleted`);
+        cardModal.onClose();
+      },
+      onError: (error) => {
+        toast.error(error);
+      },
+    }
+  );
 
   const onCopy = () => {
     const boardId = params.boardId as string;
@@ -39,10 +63,18 @@ const Actions = ({ data }: ActionsProps) => {
   return (
     <div className="space-y-2 mt-2">
       <p className="text-xs font-semibold">Actions</p>
-      <Button size="inline" className="w-full justify-start" variant="gray">
-        <IconCopy className="h-4 w-4 mr-2" /> Copy
+      <Button
+        onClick={onCopy}
+        disabled={isLoadingCopy}
+        size="inline"
+        className="w-full justify-start"
+        variant="gray"
+      >
+        <IconCopy className="h-4 w-4 mr-2" /> Duplicate
       </Button>
       <Button
+        onClick={onDelete}
+        disabled={isLoadingDelete}
         size="inline"
         className="w-full justify-start hover:bg-rose-300 text-white bg-rose-500"
         variant="gray"
