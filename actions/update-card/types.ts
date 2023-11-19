fb360a5616f7cc365d34c194f1f7fd3426494cf9
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-19 17:33:54 +0400

    feat: types for update card

--- actions/update-card/types.ts
+++ actions/update-card/types.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
-import { Board } from "@prisma/client";
+import { Card } from "@prisma/client";
 import { ActionState } from "@/lib/create-safe-action";
-import { UpdateBoard } from "./schema";
+import { UpdateCard } from "./schema";
 
-export type InputType = z.infer<typeof UpdateBoard>;
-export type ReturnType = ActionState<InputType, Board>;
+export type InputType = z.infer<typeof UpdateCard>;
+export type ReturnType = ActionState<InputType, Card>;
