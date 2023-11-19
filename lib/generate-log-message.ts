9a6647f9c6dc6770cb43a7ab50d0dbf52e8b761b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-20 01:34:06 +0400

    fix: misspel for generateLogMessage

--- lib/generate-log-message.ts
+++ lib/generate-log-message.ts
@@ -4,15 +4,12 @@ export const generateLogMessage = (log: AuditLog) => {
   const { action, entityTitle, entityType } = log;
   switch (action) {
     case ACTION.CREATE:
-      return `created ${entityType.toLowerCase()} "${entityTitle}`;
-
-    case ACTION.CREATE:
-      return `created ${entityType.toLowerCase()} "${entityTitle}`;
+      return `created ${entityType.toLowerCase()} "${entityTitle}"`;
     case ACTION.UPDATE:
-      return `updated ${entityType.toLowerCase()} "${entityTitle}`;
+      return `updated ${entityType.toLowerCase()} "${entityTitle}"`;
     case ACTION.DELETE:
-      return `deleted ${entityType.toLowerCase()} "${entityTitle}`;
+      return `deleted ${entityType.toLowerCase()} "${entityTitle}"`;
     default:
-      return `unknown action ${entityType.toLowerCase()} "${entityTitle}`;
+      return `unknown action ${entityType.toLowerCase()} "${entityTitle}"`;
   }
 };
