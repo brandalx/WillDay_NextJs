00b77141674dfc7fba80402eedb14eb423556bfb
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-20 02:03:45 +0400

    feat: decreaseAvailableCount

--- lib/orgLimits.ts
+++ lib/orgLimits.ts
@@ -25,3 +25,26 @@ export const incrementAvailableCount = async () => {
     });
   }
 };
+
+export const decreaseAvailableCount = async () => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    throw new Error("Unauthorized");
+  }
+
+  const orgLimit = await db.orgLimit.findUnique({
+    where: { orgId },
+  });
+
+  if (orgLimit) {
+    await db.orgLimit.update({
+      where: { orgId },
+      data: { count: orgLimit.count > 0 ? orgLimit.count - 1 : 0 },
+    });
+  } else {
+    await db.orgLimit.create({
+      data: { orgId, count: 1 },
+    });
+  }
+};
