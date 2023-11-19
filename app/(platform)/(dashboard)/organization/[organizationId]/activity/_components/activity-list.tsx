6a09d769394f18f0b9ab6f7119dc57de8e03e319
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-20 01:42:12 +0400

    feat: activity list ui

--- app/(platform)/(dashboard)/organization/[organizationId]/activity/_components/activity-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/_components/activity-list.tsx
@@ -1,7 +1,29 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
-
-const ActivityList = () => {
-  return <div>ActivityList</div>;
+import { db } from "@/lib/db";
+import { ActivityItem } from "@/components/activity-item";
+import { Skeleton } from "@/components/ui/skeleton";
+const ActivityList = async () => {
+  const { orgId } = auth();
+  if (!orgId) {
+    redirect("/select-org");
+  }
+  const auditLogs = await db.auditLog.findMany({
+    where: {
+      orgId,
+    },
+  });
+  return (
+    <ol className="space-y-4 mt-4">
+      <p className="hidden last:block text-xs text-center text-muted-foreground">
+        No activity found for this organization
+      </p>
+      {auditLogs.map((log) => (
+        <ActivityItem key={log.id} data={log} />
+      ))}
+    </ol>
+  );
 };
 
 export default ActivityList;
