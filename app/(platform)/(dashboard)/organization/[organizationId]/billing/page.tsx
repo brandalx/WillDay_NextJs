58850eb94767db16788c108da7ec1cbe07cc0257
brndalx <113251192+brandalx@users.noreply.github.com>
2023-11-20 04:09:39 +0400

    feat: SubscriptionButton

--- app/(platform)/(dashboard)/organization/[organizationId]/billing/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/billing/page.tsx
@@ -2,6 +2,7 @@ import { checkSubscription } from "@/lib/subscription";
 import { Separator } from "@/components/ui/separator";
 
 import { Info } from "../_components/info";
+import { SubscriptionButton } from "./_components/subscription-button";
 
 const BillingPage = async () => {
   const isPro = await checkSubscription();
@@ -10,6 +11,7 @@ const BillingPage = async () => {
     <div className="w-full">
       <Info isPro={isPro} />
       <Separator className="my-2" />
+      <SubscriptionButton isPro={isPro} />
     </div>
   );
 };
